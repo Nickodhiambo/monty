b3ab3f715a738b8d556d02dbcc973b280d595f3f
akohinmiogbegbile <akohinmiogbegbile>
2023-06-22 10:27:20 -0700

    Stacks and queues

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything
+ * @stack: stack
+ * @line_number: Line position
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
